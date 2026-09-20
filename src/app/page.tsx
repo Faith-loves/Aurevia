14dfd3996778ff08f1ccf10c42e920dfbca8a6f7
Temiloluwa Faith Kareem <omolarak724@gmail.com>
2026-09-20 01:01:15 +0100

    Show welcome and sign-in options first

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { SplashScreen } from "@/components/common/splash-screen"
 
 export default function Home() {
-  return <SplashScreen destination="/home" completedDestination="/home" />
+  return <SplashScreen destination="/welcome" completedDestination="/welcome" />
 }
